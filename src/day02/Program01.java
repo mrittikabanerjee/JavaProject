20e136bed2e6cd1888e93278f5755fee7665e369
Mrittika Banerjee <mrittikabanerjee8@gmail.com>
2023-03-27 14:09:05 +0530

    day01

---
+++ src/day02/Program01.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class Program01 {
+
+	public static void main(String args[]) {
+		
+	//public static int fun()
+	//{
+		int i= 01234567;
+		char c= 'P';
+		boolean b = true;
+		short s= 32000;
+		long l =1234567890;
+		double a1= 1234.567d;
+		float a2 = 2345.67f;
+		System.out.print(i);
+		System.out.print(a1);
+		
+	//}
+	}
+}	
---
+++ src/day02/Program02.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		 
+		// public static int fun () 
+		//{
+		  int x=10
+    System.out.println();
+		  
+		  //}
+	}
+
+}
